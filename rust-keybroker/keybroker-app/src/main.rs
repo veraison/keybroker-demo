0de4028a7345e48e57bc8d3c4b4db4fa86ae7bc4
Arnaud de Grandmaison <arnaud.degrandmaison@arm.com>
2024-09-16 18:20:42 +0200

    feat(keybroker-client): add a client library and a demo application
    The core of the functionality is provided as a library, with 2 main routines:
     - get_wrapped_key: is the core routine to get a key, with the crypto
       related to the ephemeral wrapping key left to the caller.
     - get_key: is a convenience routine which calls get_wrapped_key and handle
       the crypto when there is no specific requirement.
    
    The demo application illustrates how to use the client library to connect
    to the keybroker server and get an attestation. This enables running a
    demo of the 2 API calls needed to request the key, supply the evidence
    and RSA-decrypt the result.
    
    Co-authored-by: Paul Howard <paul.howard@arm.com>
    Signed-off-by: Arnaud de Grandmaison <arnaud.degrandmaison@arm.com>

--- rust-keybroker/keybroker-app/src/main.rs
+++ rust-keybroker/keybroker-app/src/main.rs
@@ -1,6 +1,62 @@
 // Copyright 2024 Contributors to the Veraison project.
 // SPDX-License-Identifier: Apache-2.0
 
+use clap::Parser;
+use keybroker_client::error::Error as KeybrokerError;
+use keybroker_client::{CcaExampleToken, KeyBrokerClient, TsmAttestationReport};
+use std::process;
+
+/// Structure for parsing and storing the command-line arguments
+#[derive(Clone, Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The address this client should connect to to request a key.
+    #[arg(short, long, default_value = "http://127.0.0.1:8088")]
+    endpoint: String,
+
+    /// Use a CCA example token (instead of the TSM report)
+    #[arg(short, long, default_value_t = false)]
+    mock_evidence: bool,
+
+    /// Set the application verbosity
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
+
+    /// The key name to use
+    key_name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let client = KeyBrokerClient::new(&args.endpoint, args.verbose);
+
+    let attestation_result = if args.mock_evidence {
+        client.get_key(&args.key_name, &CcaExampleToken {})
+    } else {
+        client.get_key(&args.key_name, &TsmAttestationReport {})
+    };
+
+    // If the attestation was successful, print the key we got from the keybroker and exit with code 0.
+    // If the attestation failed for genuine attestation related error, print the reason and exit with code 1.
+    // For any other kind of error (crypto, network connectivity, ...), print an hopefully useful message to diagnose the issue and exit with code 2.
+    let code = match attestation_result {
+        Ok(key) => {
+            let plainstring_key = String::from_utf8(key).unwrap();
+            println!("Attestation success :-) ! The key returned from the keybroker is '{plainstring_key}'");
+            0
+        }
+
+        Err(error) => {
+            if let KeybrokerError::AttestationFailure(reason, details) = error {
+                println!("Attestation failure :-( ! {reason}: {details}");
+                1
+            } else {
+                eprintln!("The key request failed with: {error:?}");
+                2
+            }
+        }
+    };
+
+    process::exit(code)
 }
